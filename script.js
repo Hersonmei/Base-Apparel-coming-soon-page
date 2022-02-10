ceda13f2357a326b36cd81c49f544fbcf074822f
Hersonmei <herson.me@hotmail.com>
2022-02-09 21:34:19 -0300

    Inícia do trabalho em JS

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-// const btn = document.querySelector('.btn')
+const btn = document.querySelector('.btn')
+const alert = document.querySelector('.alert')
+let email = document.getElementById("email").value;
 
-// btn.addEventListener("click", () => {
-//     console.log("Aee papai!");
-// })
+console.log(email)
+btn.addEventListener("click", () => {
+    event.preventDefault();
+
+    console.log(email);
+    // if(){
+
+    // }
+
+    alert.innerHTML = email;
+})
+
